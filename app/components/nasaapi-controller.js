e758d657081b1cb57b203227ed405520a924fc15
Madison Abegglen <madison.abegglen@gmail.com>
2018-08-14 11:52:20 -0600

    style

--- app/components/nasaapi-controller.js
+++ app/components/nasaapi-controller.js
@@ -6,44 +6,62 @@ let app = document.getElementById("app");
 function draw(data) {
   console.log(data);
   app.innerHTML = `
-    <div id="error"></div>
-    <form onsubmit="app.controllers.nasaapi.getApod(event)">
-        <label for="date">Pick a Date</label>
-        <input name="date" type="date">
-        <button type='submit'>Submit</button>
-    </form>`;
+    <nav class="navbar navbar-light bg-light">
+      <span class="navbar-brand mb-0 h1">
+        <div id="error"></div>
+        <h1>APOD - Astronomy Picture of The Day</h1>  
+          <form class="form-size" onsubmit="app.controllers.nasaapi.getApod(event)">
+            <label for="date">Pick a Date</label>
+            <input name="date" type="date">
+            <button type='submit'>Submit</button>
+          </form>
+        </div>
+      </span>
+    </nav>
+  `;
 }
 
 function drawApod(apod) {
   let apodElem = document.getElementById("main-content");
   let template = `
-    <div>
-      <p>
-        <b>Title:</b>
-        <span>${apod.title}</span>
-      </p>
-      <img src="${apod.url}" />
-      <p>
-        <b>Copyright:</b>
-        <span>${apod.copyright}</span>
-      </p>
-      <p>
-        <b>Date:</b>
-        <span>${apod.date}</span>
-      </p>
-      <p>
-        <b>Explanation:</b>
-        <span>${apod.explanation}</span>
-      </p>
-      <p>
-        <b>Media Type:</b>
-        <span>${apod.mediaType}</span>
-      </p>
-      <p>
-        <b>Service Version:</b>
-        <span>${apod.serviceVersion}</span>
-      </p>
-    </div>`;
+      <div class="row">
+        <div class="col-sm-4 mt-4">
+          <p>
+            <b>Title:</b>
+            <span>${apod.title}</span>
+          </p>
+          <img src="${apod.url}" />
+        </div>
+
+        <div class="col-sm-8 mt-4">
+          <p>
+          <b>Copyright:</b>
+          <span>${apod.copyright}</span>
+          </p>
+        </div>
+
+        <div class="col-sm-12 mt-3">
+          <p>
+          <b>Date:</b>
+          <span>${apod.date}</span>
+          </p>
+        </div>
+
+        <div class="col-sm-12 mt-3">
+          <p>
+          <b>Media Type:</b>
+          <span>${apod.mediaType}</span>
+          </p>
+        </div>
+
+        <div class="col-sm-12">
+          <p>
+          <b>Explanation:</b>
+          <span>${apod.explanation}</span>
+          </p>
+        </div>
+      </div>
+      `;
 
   apodElem.innerHTML = template;
 }
